40ed55f07284d6a1ea2e5e1290321bf9bdff75e1
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-02-18 13:08:52 +0300

    updated hero section and scss project

---
+++ src/assets/index.ts
@@ -0,0 +1,34 @@
+import hero_bg from "./images/hero-background.jpg";
+import heroDp from "./images/hero-dp.jpg";
+import logo from "./images/logo.png";
+
+import azure from "./images/tech/azure.png";
+import blazor from "./images/tech/blazor.png";
+import csharp from "./images/tech/csharp.png";
+import css from "./images/tech/css.png";
+import docker from "./images/tech/docker.png";
+import git from "./images/tech/git.png";
+import html from "./images/tech/html.png";
+import javascript from "./images/tech/javascript.png";
+import netcore from "./images/tech/netcore.png";
+import nodejs from "./images/tech/nodejs.png";
+import reactjs from "./images/tech/reactjs.png";
+import typescript from "./images/tech/typescript.png";
+
+export {
+  hero_bg,
+  heroDp,
+  logo,
+  azure,
+  blazor,
+  csharp,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  reactjs,
+  netcore,
+  nodejs,
+  typescript,
+};
